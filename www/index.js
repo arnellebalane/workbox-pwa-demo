af1c041c7f9e3242dd5f655f2da1c709c702e339
Arnelle Balane <arnellebalane@gmail.com>
2020-08-01 02:34:00 +0800

    Update demo project

--- www/index.js
+++ www/index.js
@@ -1,4 +1,10 @@
 if (navigator.serviceWorker) {
+  navigator.serviceWorker.addEventListener('message', event => {
+    if (event.data.meta === 'workbox-broadcast-update') {
+      displayUpdateAvailableUI();
+    }
+  });
+
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js');
   });
